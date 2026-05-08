ceb29b5a1505f25e9405761b89fd8ffc3bb57ab3
godwino17 <ginyang3@byupathway.edu>
2026-05-08 11:27:31 +0100

    fixed the date object in javascript

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,11 +3,11 @@ const year = new Date().getFullYear();
 document.getElementById("currentyear").innerHTML = year;
 
 const month= new Date().getMonth() + 1;
-const day= new Date().getDay();
+const date= new Date().getDate();
 const hour= new Date().getHours();
 const minute= new Date().getMinutes();
 const seconds= new Date().getSeconds();
 
-const footerP = `Last Modification: ${month}/${day}/${year} ${hour}:${minute}:${seconds}`;
+const footerP = `Last Modification: ${month}/${date}/${year} ${hour}:${minute}:${seconds}`;
 
 document.getElementById("lastmodified").innerHTML = footerP;
